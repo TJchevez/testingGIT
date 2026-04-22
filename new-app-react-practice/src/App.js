5d07dede5113007b9c38ca1106ea59e45fd4d6bb
TJchevez <cheveztj@gmail.com>
2026-04-21 21:51:51 -0400

    Looping

--- new-app-react-practice/src/App.js
+++ new-app-react-practice/src/App.js
@@ -1,6 +1,12 @@
-
-const str = "Jesus IS LORD"
-
-for(let i = 0; i < str.length; ++i) {
-  console.log(str[i])
+for (let i = 1; i <= 20; ++i) {
+  if ( i % 3 === 0 && i % 5 === 0) {
+    console.log("Frontend Simplified")
+  }
+  else if ( i % 3 === 0) {
+    console.log("Frontend")
+  }
+  else if ( i % 5 === 0) {
+    console.log("Simplified")
+  }
 }
+
